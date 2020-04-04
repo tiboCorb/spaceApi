6397f8db0749d261b4ad85db2a0928ab47c62f29
tiboCorb <corbrionthibault@gmail.com>
2020-04-05 00:27:47 +0200

    add json file to store

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { User,Status } from '../graphql.schema';
+import { User, Status } from '../graphql.schema';
+import * as fs from 'fs';
+import {resolve} from "path"
+
 
 @Injectable()
 export class UserService {
@@ -7,16 +10,19 @@ export class UserService {
     private readonly users: User[] = [{ id: 1, name: 'steve', status: Status.GodLike }];
 
     create(user: User): User {
-      user.id = this.users.length + 1;
-      this.users.push(user);
-      return user;
+        user.id = this.users.length + 1;
+        this.users.push(user);
+        return user;
     }
-  
-    findAll(): User[] {
-      return this.users;
+
+    async findAll(): Promise<User[]> {
+        const rawData = fs.readFileSync(resolve('./src/user/user.json'))
+        let user = JSON.parse(rawData.toString('utf8'));
+        return user;
     }
-  
+
     findOneById(id: number): User {
-      return this.users.find(user => user.id === id);
+        return this.users.find(user => user.id === id);
+
     }
 }
